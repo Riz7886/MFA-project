6a83c43aa1a9737b87fa40f7d0aa19510fe31ae2
syed rizvi <ra4347534@gmail.com>
2025-07-18 22:58:55 -0500

    second

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ module "compute" {
   admin_password      = var.admin_password
   subnet_ids          = module.network.subnet_ids
 }
-
+#Commit this change
 module "security" {
   source              = "./modules/security"
   location            = var.location
